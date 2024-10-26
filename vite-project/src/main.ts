1b21c07cd877c7efd40bfe9f217acc80e22783b4
Qimat Luo <qimatluo@westpharma.com>
2024-10-26 15:28:15 +0800

    Copy example
    https://css-tricks.com/styling-a-web-component/

--- vite-project/src/main.ts
+++ vite-project/src/main.ts
@@ -1,24 +1,30 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+const template = document.createElement('template');
+template.innerHTML = `
+<style>
+button {
+  background: var(--background);
+  color: var(--color);
+  padding: var(--padding);
+  font-size: var(--font-size);
+  border: 0;
+}
+</style>
+<button>Sup?</button>`;
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+class WhatsUp extends HTMLElement {
+  
+  connectedCallback() {
+    this.attachShadow({mode: 'open'});
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+    const button = this.shadowRoot.querySelector("button");
+    button.addEventListener("click", this.handleClick);
+  }
+  
+  handleClick(e) {
+    alert("Sup?");
+  }
+  
+}
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+window.customElements.define('whats-up', WhatsUp);
