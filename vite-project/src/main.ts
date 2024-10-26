cadeb218b0f456f8e192c3b5634d0d0d33e8311c
Qimat Luo <qimatluo@westpharma.com>
2024-10-26 15:39:08 +0800

    npm run format

--- vite-project/src/main.ts
+++ vite-project/src/main.ts
@@ -1,25 +1,23 @@
-import './style.css'
-import css from './component-style.css?raw'
-import html from './component-layout.html?raw'
-const template = document.createElement('template');
+import "./style.css";
+import css from "./component-style.css?raw";
+import html from "./component-layout.html?raw";
+const template = document.createElement("template");
 template.innerHTML = `
 <style>${css}</style>
 ${html}
 `;
 
 class HumanDesign extends HTMLElement {
-  
   connectedCallback() {
-    this.attachShadow({mode: 'open'});
+    this.attachShadow({ mode: "open" });
     this.shadowRoot.appendChild(template.content.cloneNode(true));
     const button = this.shadowRoot.querySelector("button");
     button.addEventListener("click", this.handleClick);
   }
-  
+
   handleClick(e) {
     alert("Sup?");
   }
-  
 }
 
-window.customElements.define('human-design', HumanDesign);
+window.customElements.define("human-design", HumanDesign);
